81a093e75a16cea0b5dffb2f701f236226195448
Michele Sousa <21959@student.dorset-college.ie>
2021-03-02 16:50:55 +0000

    first commit

---
+++ src/com/company/TitanicListing.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import com.company.models.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
+import java.util.List;
+
+public class TitanicListing extends JFrame {
+
+
+    public TitanicListing() {
+
+        String[] columns = new String[]{
+                "name", "gender", "age"
+        };
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columns);
+
+        JTable table = new JTable(model);
+
+        // model.addRow(new Object[]{"testName1", "testGender1", 24});
+        // model.addRow(new Object[]{"testName2", "testGender2", 25});
+        // model.addRow(new Object[]{"testName3", "testGender3", 26});
+
+        TitanicQueries tq = new TitanicQueries();
+        List<Passenger> passengers =  tq.getPassengersByName("james");
+
+        for(Passenger p : passengers) {
+
+            model.addRow(new Object[]{p.name, p.gender, p.age});
+        }
+
+
+
+
+        this.add(new JScrollPane(table));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+
+
+    }
+
+
+}
